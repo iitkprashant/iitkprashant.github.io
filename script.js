93254bfc1992a85bc28df82297a2e7afa4064316
Prashant <149866021+iitkprashant@users.noreply.github.com>
2024-04-20 20:17:28 +0530

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('.menu').classList.toggle('active');
-});
-function scrollToSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-    }
-}
